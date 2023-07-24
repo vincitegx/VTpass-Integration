d819ef75f8fdfeea4c12668351bfd169e586d6e9
Godstime05 <inibeheekanem0509@gmail.com>
2023-07-24 17:04:16 +0100

    first WAEC commit

---
+++ vtpass-integration/src/main/java/com/neptunesoftware/vtpassintegration/education/response/PurchaseProductResponse.java
@@ -0,0 +1,36 @@
+package com.neptunesoftware.vtpassintegration.education.response;
+
+import com.neptunesoftware.vtpassintegration.education.domain.TransactionDate;
+import org.springframework.transaction.TransactionStatus;
+
+import java.util.List;
+
+
+public record PurchaseProductResponse(
+        String code,
+        String responseDescription,
+        String requestId,
+        String amount,
+        TransactionDate transactionDate,
+        String purchasedCode,
+        List<String> tokens
+) {}
+
+
+
+
+
+
+//public class PurchaseProductResponse {
+//
+//    private String code;
+//    private String responseDescription;
+//    private String requestId;
+//    private String amount;
+//    private TransactionDate transactionDate;
+//    private String purchasedCode;
+//    private List<String> tokens;
+//
+//
+//   // private String[] tokens;
+//}
