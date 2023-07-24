04e6be1904e13223d1fc199352975a7ebf78b7d9
David Tega <davidogbodu3056@gmail.com>
2023-07-24 14:18:18 +0100

    first commit

---
+++ vtpass-integration/src/main/java/com/neptunesoftware/vtpassintegration/VtpassIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.neptunesoftware.vtpassintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtpassIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtpassIntegrationApplication.class, args);
+	}
+
+}
---
+++ vtpass-integration/src/test/java/com/neptunesoftware/vtpassintegration/VtpassIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neptunesoftware.vtpassintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtpassIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
