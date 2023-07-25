fe3dc3971307873f72f98f71f604c4b341d2a02f
James Boluwatife <bosingwa100@gmail.com>
2023-07-25 10:42:51 +0100

    new entry

--- vtpass-integration/src/main/java/com/neptunesoftware/vtpassintegration/airtime/response/AirtimeResponse.java
+++ vtpass-integration/src/main/java/com/neptunesoftware/vtpassintegration/airtime/response/AirtimeResponse.java
@@ -9,5 +9,56 @@ public class AirtimeResponse {
 
     private Integer amount;
 
+    public AirtimeResponse(Integer code, String requestId, Integer transactionId, Integer amount, TransactionDate transaction_date) {
+        this.code = code;
+        this.requestId = requestId;
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.transaction_date = transaction_date;
+    }
+
+    public AirtimeResponse() {
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public Integer getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Integer transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public TransactionDate getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(TransactionDate transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
     private TransactionDate transaction_date;
 }
