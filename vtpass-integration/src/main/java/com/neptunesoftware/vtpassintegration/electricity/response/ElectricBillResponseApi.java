5e58d6d2690d1fb578366e04f12f50172ba2c2ca
IMOBI LIBERTY UGOCHUKWU <libertyimobi@gmail.com>
2023-07-31 16:33:44 +0100

    Updated tv and electric module

---
+++ vtpass-integration/src/main/java/com/neptunesoftware/vtpassintegration/electricity/response/ElectricBillResponseApi.java
@@ -0,0 +1,15 @@
+package com.neptunesoftware.vtpassintegration.electricity.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ElectricBillResponseApi<T> {
+private ElectricPrepaidResponse electricPrepaidResponse;
+private ElectricPostpaidResponse electricPostpaidResponse;
+}
