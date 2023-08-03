ff730b2e9d339106590808d35bb7e0bb14c6cd8c
David Tega <davidogbodu3056@gmail.com>
2023-08-03 16:21:13 +0100

    New Commit

--- vtpass-integration/src/main/java/com/neptunesoftware/vtpassintegration/commons/service/ProductOptionsService.java
+++ vtpass-integration/src/main/java/com/neptunesoftware/vtpassintegration/commons/service/ProductOptionsService.java
@@ -1,6 +1,6 @@
 package com.neptunesoftware.vtpassintegration.commons.service;
 
-import com.neptunesoftware.vtpassintegration.commons.response.AeroResponse;
+import com.neptunesoftware.vtpassintegration.commons.response.ProductOptionsResponse;
 import com.neptunesoftware.vtpassintegration.config.Credentials;
 import com.neptunesoftware.vtpassintegration.transaction.exception.TransactionException;
 import lombok.RequiredArgsConstructor;
@@ -9,12 +9,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
-public class AeroService {
+public class ProductOptionsService {
 
     private final Credentials credentials;
     private final WebClient.Builder webClientBuilder;
 
-    public AeroResponse getProductOptionsForAeroService(String serviceID, String name){
+    public ProductOptionsResponse getProductOptionsForAeroService(String serviceID, String name){
         try{
             return webClientBuilder.build().get()
                     .uri(credentials.getBaseUrl()+"/api/options",
@@ -24,7 +24,7 @@ public class AeroService {
                     .header("api-key", credentials.getApiKey())
                     .header("secret-key", credentials.getSecretKey())
                     .retrieve()
-                    .bodyToMono(AeroResponse.class)
+                    .bodyToMono(ProductOptionsResponse.class)
                     .block();
         }catch (Exception ex){
             throw new TransactionException("PRODUCT DOES NOT EXIST", "012", null);
