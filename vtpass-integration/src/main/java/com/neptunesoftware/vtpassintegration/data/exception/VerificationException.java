74b710c6e96cda7a3ec4bdc0d8ec0aef6bb056b5
David Tega <davidogbodu3056@gmail.com>
2023-08-03 11:56:47 +0100

    New Commit

--- vtpass-integration/src/main/java/com/neptunesoftware/vtpassintegration/data/exception/VerificationException.java
+++
@@ -1,7 +0,0 @@
-package com.neptunesoftware.vtpassintegration.data.exception;
-
-public class VerificationException extends RuntimeException{
-    public VerificationException(String message) {
-        super(message);
-    }
-}
